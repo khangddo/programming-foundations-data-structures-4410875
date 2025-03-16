84cf9a820a919c0c53efdf200c3783c0c7377629
Ken Do <129320171+khangddo@users.noreply.github.com>
2025-03-16 02:35:53 +0000

    Data Structures

--- 02/02_09/02_09e/main.py
+++ 02/02_09/02_09e/main.py
@@ -1,4 +1,15 @@
 def find_second_smallest(my_list):
-    return 0
+    if (len(my_list) < 2):
+        return None
+    
+    smallest = float('inf')
+    second_smallest = float('inf')
+    for val in my_list:      
+        if (val < smallest):
+            second_smallest = smallest
+            smallest = val
+        elif (val < second_smallest):
+            second_smallest = val
+    return second_smallest
 
-print(find_second_smallest([5, 8, 3, 2, 6]))
+print(find_second_smallest([1, 0, 2, 3, 4, 5, 6]))
